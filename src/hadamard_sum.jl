802d6f9773900f158f2c67c1698226ae1edffad9
Jesse Chan <jchan985@gmail.com>
2021-01-23 15:02:50 -0600

    no-alloc specialization for hadamard sum

--- src/hadamard_sum.jl
+++ src/hadamard_sum.jl
@@ -1,5 +1,9 @@
-# helper functions
-bmult(a,b) = a.*b # broadcasted multiplication
+"""
+    TupleOrSVector{N}
+
+Either a NTuple or SVector (e.g., fast static container) of length N.
+"""
+const TupleOrSVector{N} = Union{NTuple{N,T},SVector{N,T}} where {T}
 
 #####
 ##### routine works for both dense/sparse matrix routines
@@ -13,7 +17,7 @@ computes ∑_i sum(Ai.*Fi,dims=2) where (Fi)_jk = F(uj,uk)[i]
 
 Inputs
 - `A_list`: tuple (or similar container) of operators (A1,...,Ad)
-- `F`: flux function which outputs a d-tuple of flux vectors
+- `F`: flux function which outputs a length `d` container of flux vectors
 - `u`: collection of solution values (or arrays) at which to evaluate `F`
 - `Fargs`: extra arguments to `F(ui,uj,getindex.(Fargs,i)...,getindex.(Fargs,j)...)`
 - (optional) `skip_index(i,j)==true` skips computing fluxes for index (i,j)
@@ -33,30 +37,31 @@ end
     hadamard_sum_ATr!(rhs,ATr_list::NTuple{N,SparseMatrixCSC},F,u,Fargs...) where {N}
     hadamard_sum_ATr!(rhs,ATr::SparseMatrixCSC,F,u,Fargs...)
 
-Same as [`hadamard_sum!`](@ref) but `ATr_list` contains transposed matrices.
+Same as [`hadamard_sum`](@ref) except that `hadamard_sum_ATr!`
+- assumes `ATr_list` contains transposed matrices.
+- accumulates the result into `rhs`
 Specializes based on whether `ATr_list` contains SparseMatrixCSC or general arrays.
-SparseMatrixCSC works best if the matrices in `ATr_list` have distinct sparsity patterns.
+The SparseMatrixCSC version works best if the matrices in `ATr_list` have distinct sparsity patterns.
 """
 function hadamard_sum_ATr!(rhs,ATr_list,F::Fxn,u,Fargs...; skip_index=(i,j)->false) where {Fxn}
-    rhstype = eltype(first(rhs))
-    val_i = zeros(rhstype,length(rhs))
+    val_i = zeros(eltype(first(rhs)),length(rhs)) # preallocate array
     rows,cols = axes(first(ATr_list))
     for i in cols
         ui = getindex.(u,i)
-        Fargs_i = getindex.(Fargs,i)
-        fill!(val_i,zero(rhstype))
+        val_i .= getindex.(rhs,i)
         for j in rows
             if skip_index(i,j)==false
                 uj = getindex.(u,j)
                 ATrij_list = getindex.(ATr_list,j,i)
-                Fij = F(ui,uj,Fargs_i...,getindex.(Fargs,j)...)
-                val_i .+= sum(bmult.(ATrij_list,Fij))
+                Fij = F(ui,uj,getindex.(Fargs,i)...,getindex.(Fargs,j)...)
+                val_i .+= sum(map((x,y)->x.*y,ATrij_list,Fij))
             end
         end
         setindex!.(rhs,val_i,i)
     end
 end
 
+# version for a list of sparse matrices (loop thru them)
 function hadamard_sum_ATr!(rhs,ATr_list::NTuple{N,SparseMatrixCSC},F::Fxn,u,Fargs...) where {N,Fxn}
     for (i,ATr) in enumerate(ATr_list)
         F_i = (x->getindex(x,i)) ∘ F
@@ -64,6 +69,7 @@ function hadamard_sum_ATr!(rhs,ATr_list::NTuple{N,SparseMatrixCSC},F::Fxn,u,Farg
     end
 end
 
+# version for a single sparse matrix
 function hadamard_sum_ATr!(rhs,ATr::SparseMatrixCSC,F::Fxn,u,Fargs...) where {Fxn}
     val_i = zeros(eltype(first(rhs)),length(rhs))
     rows = rowvals(ATr)
@@ -83,21 +89,33 @@ function hadamard_sum_ATr!(rhs,ATr::SparseMatrixCSC,F::Fxn,u,Fargs...) where {Fx
 end
 
 """
-    TupleOrSVector{N}
-
-Either a NTuple or SVector (e.g., fast static container) of length N.
-"""
-const TupleOrSVector{N} = Union{NTuple{N,T},SVector{N,T}} where {T}
-
-"""
+    hadamard_sum_ATr!(rhs::TupleOrSVector{N},ATr_list::NTuple{D,Array},F::Fxn,u,Fargs...;
+                      skip_index=(i,j)->false) where {N,D,Fxn}
     hadamard_sum_ATr!(rhs::NTupleOrSVector{N},ATr::SparseMatrixCSC,F::Fxn,u,Fargs...) where {N,Fxn}
 
-Zero-allocation version if rhs has a statically inferrable length (e.g., is an NTuple or SVector)
+Zero-allocation dense/sparse versions if rhs has a statically inferrable length (e.g., is an NTuple or SVector)
 """
+function hadamard_sum_ATr!(rhs::TupleOrSVector{N},ATr_list::NTuple{D,Array},F::Fxn,u,Fargs...;
+                           skip_index=(i,j)->false) where {N,D,Fxn}
+    rows,cols = axes(first(ATr_list))
+    for i in cols
+        ui = getindex.(u,i)
+        val_i = MVector{N}(getindex.(rhs,i)) # accumulate into existing rhs
+        for j in rows
+            if skip_index(i,j)==false
+                uj = getindex.(u,j)
+                ATrij_list = getindex.(ATr_list,j,i)
+                Fij = F(ui,uj,getindex.(Fargs,i)...,getindex.(Fargs,j)...)
+                val_i .+= sum(map((x,y)->x.*y,ATrij_list,Fij))
+            end
+        end
+        setindex!.(rhs,val_i,i)
+    end
+end
 function hadamard_sum_ATr!(rhs::TupleOrSVector{N},ATr::SparseMatrixCSC,F::Fxn,u,Fargs...) where {N,Fxn}
     rows = rowvals(ATr)
     vals = nonzeros(ATr)
-    for i = 1:size(ATr,2) # all ops should be same length
+    for i = 1:size(ATr,2)
         ui = getindex.(u,i)
         val_i = MVector{N}(getindex.(rhs,i)) # accumulate into existing rhs
         for row_id in nzrange(ATr,i)
